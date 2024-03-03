2d83cf8feb98c001e85523a6fd0d78217d81a5b5
JackyZhang <j564748990@sina.com>
2024-03-03 21:33:53 +0800

    add https support

--- src/main/java/com/example/demo/Config/StorageDirConfig.java
+++ src/main/java/com/example/demo/Config/StorageDirConfig.java
@@ -3,6 +3,9 @@ package com.example.demo.Config;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+/**
+ * StorageDirConfig 类用于配置存储目录。
+ */
 public class StorageDirConfig {
     public enum DIR  {
         IMAGES ,
